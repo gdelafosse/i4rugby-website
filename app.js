63f9ff80e8f950d9eb949259f90d165a161c55c3
Guillaume Delafosse <gdelafosse@axway.com>
2016-05-19 22:27:37 +0200

    set port with environment variable

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 const express = require('express');
 const app = express();
 
+app.set('port', (process.env.PORT || 8080))
+
 app.use(express.static('www'));
 
-app.listen(8080, () => {
+app.listen(app.get('port'), () => {
   console.log('i4rugby website is running!');
 });
